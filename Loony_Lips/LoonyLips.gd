8d878e5925c3bd0ade363194da9c3a66014b3d59
andredarcie <andrendarcie@gmail.com>
2021-01-08 23:39:51 -0300

    Show text to the player

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -3,4 +3,5 @@ extends Control
 func _ready():
 	var prompts = ["Yann", "Minions", "greatest"]
 	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past two decades"
-	print(story % prompts)
+	
+	$DisplayText.text = story % prompts
