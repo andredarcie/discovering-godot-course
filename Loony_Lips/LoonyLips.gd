61413ab3f41685daefbe951b32ee5dd7330f62f1
andredarcie <andrendarcie@gmail.com>
2021-01-09 15:18:28 -0300

    Get text from the player

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -1,7 +1,18 @@
 extends Control
 
+onready var displayText = $VBoxContainer/DisplayText
+onready var playerText = $VBoxContainer/PlayerText
+
 func _ready():
 	var prompts = ["Yann", "Minions", "greatest"]
 	var story = "Once upon a time %s watched %s and thought it was the %s movie of the past two decades"
-	
-	$DisplayText.text = story % prompts
+	displayText.text = story % prompts
+
+
+func _on_PlayerText_text_entered(new_text):
+	update_DisplayText(new_text)
+
+
+func update_DisplayText(words: String):
+	displayText.text = words
+	playerText.clear()
