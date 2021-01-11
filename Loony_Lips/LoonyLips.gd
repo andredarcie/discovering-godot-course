c9d70f3c8a471fb2f14bad3afce1f0e1ff10b0d2
andredarcie <andrendarcie@gmail.com>
2021-01-10 23:09:12 -0300

    Read story from json

--- Loony_Lips/LoonyLips.gd
+++ Loony_Lips/LoonyLips.gd
@@ -17,13 +17,27 @@ func _ready():
 
 
 func pick_current_story():
+	var stories = get_from_json("StoryBook.json")
 	randomize()
+	current_story = stories[randi() % stories.size()]
+
+
+func get_from_json(filename):
+	var file = File.new()
+	file.open(filename, File.READ)
+	var text = file.get_as_text()
+	var data = parse_json(text)
+	file.close()
+	return data
+	
+
+func get_from_nodes():
 	var stories = $StoryBook.get_child_count()
 	var selected_story = randi() % stories
 	current_story.prompts = $StoryBook.get_child(selected_story).prompts
 	current_story.story = $StoryBook.get_child(selected_story).story
 	
-
+	
 func _on_PlayerText_text_entered(new_text):
 	add_to_player_words()
 
