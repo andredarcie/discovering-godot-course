ff293267b4d4ce865f76d1403ce458fbbf385676
andredarcie <andrendarcie@gmail.com>
2021-01-28 22:05:03 -0300

    Add coins

--- Hoppy Days/GUI/GUI.gd
+++ Hoppy Days/GUI/GUI.gd
@@ -3,6 +3,7 @@ extends CanvasLayer
 func _ready():
 	$Control/TextureRect/HBoxContainer/LifeCount.text = "3"
 	
-func update_lives(lives_left):
+func update_GUI(lives_left, coins):
 	$Control/TextureRect/HBoxContainer/LifeCount.text = str(lives_left)
+	$Control/TextureRect/HBoxContainer/CoinCount.text = str(coins)
 	
