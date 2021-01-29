ac9cc1e4ba8cc774028711c555a85aab43249fcd
andredarcie <andrendarcie@gmail.com>
2021-01-28 21:35:04 -0300

    Add GUI

--- Hoppy Days/Levels/GameState.gd
+++ Hoppy Days/Levels/GameState.gd
@@ -4,14 +4,19 @@ var lives = 3
 
 func _ready():
 	add_to_group("Gamestate")
+	update_GUI()
 	
 	
 func hurt():
 	lives -= 1
 	$Player.hurt()
+	update_GUI()
 	if lives < 0:
 		end_game()
 		
-
+		
+func update_GUI():
+	get_tree().call_group("GUI", "update_lives", lives)
+	
 func end_game():
 	get_tree().change_scene("res://Levels/EndGame.tscn")
